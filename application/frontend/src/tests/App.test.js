9adedc27f2863cc9d3640652cc095a8a7b77ffa1
Yonna Khatri <yonnakhatri@gmail.com>
2025-03-24 18:51:08 +0000

    add tests for dashboard, app, welcome, axios instance

--- application/frontend/src/tests/App.test.js
+++ application/frontend/src/tests/App.test.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
+jest.mock('@fullcalendar/react', () => (props) => (
+    <div>
+      <button onClick={props.customButtons?.addEventButton?.click}>Add Event</button>
+      {props.events?.map((event) => (
+        <div key={event.id}>{event.title}</div>
+      ))}
+    </div>
+  ));
+  
+jest.mock('@fullcalendar/daygrid', () => () => <div>Mocked DayGridPlugin</div>);
+jest.mock('@fullcalendar/timegrid', () => () => <div>Mocked TimeGridPlugin</div>);
 jest.mock('../firebase-config.js') ;
+jest.mock('../assets/thestudyspot.jpeg', () => 'thestudyspot.jpeg');
+jest.mock('../assets/blueberry.jpeg', () => 'blueberry.jpeg');
+jest.mock('../assets/generate.PNG', () => 'generate.PNG');
+
 test('renders welcome heading', () => {
     render(<App />);
     const headingElement = screen.getByText(/The Study Spot/i); // Or any text present in Welcome.js
