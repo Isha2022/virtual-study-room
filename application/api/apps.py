1d16184638f669c88d11d7201cf9becc103d76ec
Prapti <prapti1713@gmail.com>
2025-03-26 14:22:48 +0000

    COMMENT/CLEAN api folder

--- application/api/apps.py
+++ application/api/apps.py
@@ -2,5 +2,10 @@ from django.apps import AppConfig
 
 
 class ApplicationConfig(AppConfig):
+    """Configuration class for the 'api' Django application."""
+
+    # Use BigAutoField as default primary key type for models
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Name of the Django application (must match package name)
     name = 'api'
